636b433e004f8b7d21a1b4c4c76bb4e8c232a357
maronv64 <maron.v64@gmail.com>
2019-07-19 15:56:59 -0500

    inicio

---
+++ database/factories/MensajeFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Mensaje;
+use Faker\Generator as Faker;
+
+$factory->define(Mensaje::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
